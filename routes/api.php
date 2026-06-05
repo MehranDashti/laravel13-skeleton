d8ff72ac5b37aabee1e21f37405a7fee826f0d5c
Mehran <mehran19.md@gmail.com>
2026-06-05 21:37:06 +0330

    fix: Fix method name

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\HealthCheckController;
 
 // Health
-Route::get('/health-check', [HealthCheckController::class, 'index']);
+Route::get('/health-check', [HealthCheckController::class, 'healthCheck']);
